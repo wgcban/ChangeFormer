e9e61706833e80db34ecdc3e814bfc9d3997f343
wgcban <w.chamindabandara@gmail.com>
2021-12-27 12:14:52 -0500

    bug fixes for eval

--- eval_cd.py
+++ eval_cd.py
@@ -32,6 +32,7 @@ def main():
 
     # model
     parser.add_argument('--n_class', default=2, type=int)
+    parser.add_argument('--embed_dim', default=256, type=int)
     parser.add_argument('--net_G', default='base_transformer_pos_s4_dd8_dedim8', type=str,
                         help='base_resnet18 | base_transformer_pos_s4_dd8 | base_transformer_pos_s4_dd8_dedim8|')
 
