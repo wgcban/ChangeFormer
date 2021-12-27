c1b425301a6fdbe0bdfe0bf99185c4cc835ec02d
wgcban <w.chamindabandara@gmail.com>
2021-12-27 12:11:54 -0500

    Added test scripts

---
+++ scripts/eval_ChangeFormer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+gpus=0
+
+data_name=LEVIR
+net_G=ChangeFormerV5
+split=test
+vis_root=/media/lidan/ssd2/ChangeFormer/vis
+project_name=CD_ChangeFormerV5_LEVIR_b16_lr0.01_sgd_train_test_200_linear_ce_multi_train_False_multi_infer_False_shuffle_AB_False_embed_dim_256
+checkpoints_root=/media/lidan/ssd2/ChangeFormer/checkpoints
+checkpoint_name=best_ckpt.pt
+img_size=256
+embed_dim=256 #Make sure to change the embedding dim (best and default = 256)
+
+python eval_cd.py --split ${split} --net_G ${net_G} --embed_dim ${embed_dim} --img_size ${img_size} --vis_root ${vis_root} --checkpoints_root ${checkpoints_root} --checkpoint_name ${checkpoint_name} --gpu_ids ${gpus} --project_name ${project_name} --data_name ${data_name}
+
+
