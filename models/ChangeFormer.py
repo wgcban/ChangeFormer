a2e0fb690c494677a92295a07559741ea38e8d7b
wgcban <w.chamindabandara@gmail.com>
2021-12-28 00:18:57 -0500

    ChangeFormerV6

--- models/ChangeFormer.py
+++ models/ChangeFormer.py
@@ -1342,11 +1342,11 @@ class EncoderTransformer_v3(nn.Module):
         # patch embedding definitions
         self.patch_embed1 = OverlapPatchEmbed(img_size=img_size, patch_size=7, stride=4, in_chans=in_chans,
                                               embed_dim=embed_dims[0])
-        self.patch_embed2 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=3, stride=2, in_chans=embed_dims[0],
+        self.patch_embed2 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=patch_size, stride=2, in_chans=embed_dims[0],
                                               embed_dim=embed_dims[1])
-        self.patch_embed3 = OverlapPatchEmbed(img_size=img_size // 8, patch_size=3, stride=2, in_chans=embed_dims[1],
+        self.patch_embed3 = OverlapPatchEmbed(img_size=img_size // 8, patch_size=patch_size, stride=2, in_chans=embed_dims[1],
                                               embed_dim=embed_dims[2])
-        self.patch_embed4 = OverlapPatchEmbed(img_size=img_size // 16, patch_size=3, stride=2, in_chans=embed_dims[2],
+        self.patch_embed4 = OverlapPatchEmbed(img_size=img_size // 16, patch_size=patch_size, stride=2, in_chans=embed_dims[2],
                                               embed_dim=embed_dims[3])
 
         # Stage-1 (x1/4 scale)
@@ -1635,6 +1635,41 @@ class ChangeFormerV5(nn.Module):
 
         self.Tenc_x2    = EncoderTransformer_v3(img_size=256, patch_size = 4, in_chans=input_nc, num_classes=output_nc, embed_dims=self.embed_dims,
                  num_heads = [1, 2, 5, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=True, qk_scale=None, drop_rate=self.drop_rate,
+                 attn_drop_rate = self.attn_drop, drop_path_rate=self.drop_path_rate, norm_layer=partial(nn.LayerNorm, eps=1e-6),
+                 depths=self.depths, sr_ratios=[8, 4, 2, 1])
+        
+        #Transformer Decoder
+        self.TDec_x2   = DecoderTransformer_v3(input_transform='multiple_select', in_index=[0, 1, 2, 3], align_corners=False, 
+                    in_channels = self.embed_dims, embedding_dim= self.embedding_dim, output_nc=output_nc, 
+                    decoder_softmax = decoder_softmax, feature_strides=[2, 4, 8, 16])
+
+    def forward(self, x1, x2):
+
+        [fx1, fx2] = [self.Tenc_x2(x1), self.Tenc_x2(x2)]
+
+        cp = self.TDec_x2(fx1, fx2)
+
+        # # Save to mat
+        # save_to_mat(x1, x2, fx1, fx2, cp, "ChangeFormerV4")
+
+        # exit()
+        return cp
+
+# ChangeFormerV5:
+class ChangeFormerV6(nn.Module):
+
+    def __init__(self, input_nc=3, output_nc=2, decoder_softmax=False, embed_dim=256):
+        super(ChangeFormerV6, self).__init__()
+        #Transformer Encoder
+        self.embed_dims = [64, 128, 320, 512]
+        self.depths     = [3, 3, 4, 3] #[3, 3, 6, 18, 3]
+        self.embedding_dim = embed_dim
+        self.drop_rate = 0.1
+        self.attn_drop = 0.1
+        self.drop_path_rate = 0.1 
+
+        self.Tenc_x2    = EncoderTransformer_v3(img_size=256, patch_size = 7, in_chans=input_nc, num_classes=output_nc, embed_dims=self.embed_dims,
+                 num_heads = [1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=True, qk_scale=None, drop_rate=self.drop_rate,
                  attn_drop_rate = self.attn_drop, drop_path_rate=self.drop_path_rate, norm_layer=partial(nn.LayerNorm, eps=1e-6),
                  depths=self.depths, sr_ratios=[8, 4, 2, 1])
         
