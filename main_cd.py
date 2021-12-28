e3f46425c7fab30934255412ec14fd75568f583c
wgcban <w.chamindabandara@gmail.com>
2021-12-28 12:30:27 -0500

    Fix for number of workers

--- main_cd.py
+++ main_cd.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     parser.add_argument('--vis_root', default='vis', type=str)
 
     # data
-    parser.add_argument('--num_workers', default=4, type=int)
+    parser.add_argument('--num_workers', default=2, type=int)
     parser.add_argument('--dataset', default='CDDataset', type=str)
     parser.add_argument('--data_name', default='LEVIR', type=str)
 
