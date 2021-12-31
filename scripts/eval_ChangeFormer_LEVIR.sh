973f4e6043f4f80863a9070d8644b95501875c6a
wgcban <chaminda.bandara@eng.pdn.ac.lk>
2021-12-31 11:35:40 -0500

    update eval code

--- scripts/eval_ChangeFormer_LEVIR.sh
+++ scripts/eval_ChangeFormer_LEVIR.sh
@@ -12,6 +12,6 @@ checkpoint_name=best_ckpt.pt
 img_size=256
 embed_dim=256 #Make sure to change the embedding dim (best and default = 256)
 
-python eval_cd.py --split ${split} --net_G ${net_G} --embed_dim ${embed_dim} --img_size ${img_size} --vis_root ${vis_root} --checkpoints_root ${checkpoints_root} --checkpoint_name ${checkpoint_name} --gpu_ids ${gpus} --project_name ${project_name} --data_name ${data_name}
+CUDA_VISIBLE_DEVICES=0 python eval_cd.py --split ${split} --net_G ${net_G} --embed_dim ${embed_dim} --img_size ${img_size} --vis_root ${vis_root} --checkpoints_root ${checkpoints_root} --checkpoint_name ${checkpoint_name} --gpu_ids ${gpus} --project_name ${project_name} --data_name ${data_name}
 
 
