3f6b415b6ba735fb6806c2a7c989debd73d96463
wgcban <chaminda.bandara@eng.pdn.ac.lk>
2022-01-03 19:57:22 -0500

    added DSIFN for run BIT

--- scripts/run_SOTA/run_BIT.sh
+++ scripts/run_SOTA/run_BIT.sh
@@ -3,7 +3,7 @@
 gpus=0
 checkpoint_root=/media/lidan/ssd2/ChangeFormer/checkpoints
 vis_root=/media/lidan/ssd2/ChangeFormer/vis
-data_name=LEVIR
+data_name=DSIFN #LEVIR, DSIFN
 
 img_size=256
 batch_size=8
@@ -19,4 +19,4 @@ split=train
 split_val=test
 project_name=CD_${net_G}_${data_name}_b${batch_size}_lr${lr}_${split}_${split_val}_${max_epochs}_${lr_policy}
 
-CUDA_VISIBLE_DEVICES=1 python main_cd.py --img_size ${img_size} --checkpoint_root ${checkpoint_root} --vis_root ${vis_root} --lr_policy ${lr_policy} --split ${split} --split_val ${split_val} --net_G ${net_G} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --data_name ${data_name}  --lr ${lr}
+CUDA_VISIBLE_DEVICES=0 python main_cd.py --img_size ${img_size} --checkpoint_root ${checkpoint_root} --vis_root ${vis_root} --lr_policy ${lr_policy} --split ${split} --split_val ${split_val} --net_G ${net_G} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --data_name ${data_name}  --lr ${lr}
